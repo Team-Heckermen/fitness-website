bc7ab134798ff08e4cf9c750fa02bf87bb8a20d1
Dheer <dheerm309@gmail.com>
2021-08-25 17:26:43 +0530

    made the user schema

--- api/models.py
+++ api/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 
-# Create your models here.
+class UserAccountManager(BaseUserManager):
+    def create_user(self, email, username, password=None):
+        if not email:
+            raise ValueError('You must enter your email!')
+
+        if not username:
+            raise ValueError('You must enter your username!')
+
+        user = self.model(
+            email=self.normalize_email(email),
+            username=username,
+        )
+
+        user.set_password(password)
+        user.save()
+
+        return user
+
+    def create_superuser(self, email, username, password=None):
+        user = self.create_user(
+            email=self.normalize_email(email),
+            username=username
+            password=password,
+        )
+        user.is_superuser = True
+        user.is_admin = True
+        user.is_staff = True
+
+        user.save()
+
+        return user
+
+
+class UserAccount(AbstractBaseUser, PermissionMixin):
+    email = models.EmailField(max_length=255, unique=True)
+    username = models.CharField(max_length=255, unique=True)
+    is_active = models.BooleanField(default=True)
+    is_superuser = models.BooleanField(default=False)
+
+    objects = UserAccountManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'email']
+
+    def __str__(self):
+        return self.email
+
