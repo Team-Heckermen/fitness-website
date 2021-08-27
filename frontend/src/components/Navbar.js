5bfe77fdd51afdb4b64c3891bca990ea58280472
madara uchiha <pritanshsahsani@gmail.com>
2021-08-27 12:17:27 +0530

    eddited navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -5,23 +5,46 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 export default function Navbar(props) {
   const is_logged_in = false;
-  var discussion = <li>
-  <Link
-    to="/"
-    className=" rounded-md my-2 inline-block hover:bg-gray-700 no-underline bg-gray-800 p-1 text-gray-400 hover:text-white font-medium text-lg py-2 px-4 lg:-ml-2 mx-6"
-  >
-    Discussions
-  </Link>
+  var home = 
+  <li>
+    <Link
+      to="/"
+      className=" rounded-md my-2 hover:bg-gray-700 inline-block no-underline hover:text-white bg-gray-800 p-1 text-gray-400 font-medium text-lg py-2 px-4 mr-6"
+    >
+      Home
+    </Link>
   </li>
 
-  var home = <li>
-  <Link
-    to="/"
-    className=" rounded-md my-2 hover:bg-gray-700 inline-block no-underline hover:text-white bg-gray-800 p-1 text-gray-400 font-medium text-lg py-2 px-4 mr-6"
-  >
-    Home
-  </Link>
+  var home_open = 
+  <li>
+    <Link
+      to="/"
+      className=" rounded-md my-2 hover:bg-gray-700 inline-block no-underline hover:text-white bg-gray-800 p-1 text-gray-400 font-medium text-lg py-2 px-4 mr-6"
+    >
+      Home
+    </Link>
   </li>
+
+  var discussion = 
+  <li>
+    <Link
+      to="/"
+      className=" rounded-md my-2 inline-block hover:bg-gray-700 no-underline bg-gray-800 p-1 text-gray-400 hover:text-white font-medium text-lg py-2 px-4 lg:-ml-2 mx-6"
+    >
+      Discussions
+    </Link>
+  </li>
+
+  var discussion_open = 
+  <li>
+    <Link
+      to="/"
+      className=" rounded-md my-2 hover:bg-gray-700 inline-block no-underline hover:text-white bg-gray-800 p-1 text-gray-400 font-medium text-lg py-2 px-4 mr-6"
+    >
+      Discissions
+    </Link>
+  </li>
+
   var settings = 
   <li>
     <Link
@@ -32,8 +55,19 @@ export default function Navbar(props) {
     </Link>
   </li>
 
+  var settings_open =
+  <li>
+    <Link
+    to="/"
+    className=" rounded-md my-2 hover:bg-gray-700 inline-block no-underline hover:text-white bg-gray-800 p-1 text-gray-400 font-medium text-lg py-2 px-4 mr-6"
+    >
+      settings
+    </Link>
+  </li>
+
   if (is_logged_in == true) {
     var sign_in;
+    var sign_in_open;
     var sign_up = (
       <li className="absolute right-0">
         <Link
@@ -44,8 +78,29 @@ export default function Navbar(props) {
         </Link>
       </li>
     );
-  } else {
+    var sign_up_open = (
+      <li className="absolute right-0">
+        <Link
+          to="/"
+          className=" rounded-md my-2 hover:bg-gray-700 inline-block no-underline bg-gray-800 p-1 text-gray-400 hover:text-white font-medium text-lg py-2 px-4 lg:-ml-2 mx-6"
+        >
+          Profile
+        </Link>
+      </li>
+    );
+  }
+  else {
     var sign_in = (
+      <li className="absolute right-24">
+        <Link
+          to="/"
+          className=" rounded-md my-2 hover:bg-gray-700 inline-block no-underline hover:text-white bg-gray-800 p-1 text-gray-400 font-medium text-lg py-2 px-4 lg:-ml-2 mx-6"
+        >
+          Sign in
+        </Link>
+      </li>
+    );
+    var sign_in_open = (
       <li className="absolute right-24">
         <Link
           to="/"
@@ -65,25 +120,71 @@ export default function Navbar(props) {
         </Link>
       </li>
     );
+    var sign_up_open = (
+      <li className="absolute right-0">
+        <Link
+          to="/"
+          className=" rounded-md my-2 hover:bg-gray-700 inline-block no-underline bg-gray-800 p-1 text-gray-400 hover:text-white font-medium text-lg py-2 px-4 lg:-ml-2 mx-6"
+        >
+          Sign up
+        </Link>
+      </li>
+    );
   }
   return (
-    <header className="hidden md:flex md:items-center md:w-auto w-full order-3 md:order-1">
-      <nav className="bg-gray-800 h-auto w-screen content-start">
-        <ul className="flow-root md:flex items-center text-base text-blue-600 pt-4 md:pt-0 w-screen">
-          <img
-            src={logo}
-            className="inline-block mr-4"
-            height="5%"
-            width="5%"
-          />
-          {home}
-          {discussion}
-          {settings}
-          {sign_in}
-          {sign_up}
-          {/* <img src={userimg} className="inline-block mr-4 absolute right-0" height="4%" width="4%"/> */}
-        </ul>
-      </nav>
-    </header>
+    <Router>
+      <header className="hidden md:flex md:items-center md:w-auto w-full order-3 md:order-1">
+        <nav className="bg-gray-800 h-auto w-screen content-start">
+          <ul className="flow-root md:flex items-center text-base text-blue-600 pt-4 md:pt-0 w-screen">
+            <Link to="/">
+              <img
+                src={logo}
+                className="inline-block mr-4"
+                height="5%"
+                width="5%"
+              />
+            </Link>
+            <Switch>
+            <Route exact path="/">
+              {home_open}
+              {discussion}
+              {settings}
+              {sign_in}
+              {sign_up}
+            </Route>
+            <Route exact path="/discussions">
+              {home}
+              {discussion_open}
+              {settings}
+              {sign_in}
+              {sign_up}
+            </Route>
+            <Route exact path="/settings">
+              {home}
+              {discussion}
+              {settings_open}
+              {sign_in}
+              {sign_up}
+            </Route>
+            <Route exact path="/sign_in">
+              {home}
+              {discussion}
+              {settings}
+              {sign_in_open}
+              {sign_up}
+            </Route>
+            <Route exact path="/sign_up">
+              {home}
+              {discussion}
+              {settings}
+              {sign_in}
+              {sign_up_open}
+            </Route>
+          </Switch>
+            {/* <img src={userimg} className="inline-block mr-4 absolute right-0" height="4%" width="4%"/> */}
+          </ul>
+        </nav>
+      </header>
+    </Router>
   );
 }
