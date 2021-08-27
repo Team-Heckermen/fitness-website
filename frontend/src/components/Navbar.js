f7479fa3f62ce40420edec6f5fb98e811fdc0416
madara uchiha <pritanshsahsani@gmail.com>
2021-08-27 18:16:16 +0530

    navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -6,70 +6,64 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 export default function Navbar(props) {
   const is_logged_in = false;
   var home = (
-    <li>
-      <Link
-        to="/"
-        className=" rounded-md my-2 hover:bg-gray-700 inline-block no-underline hover:text-white bg-gray-800 p-1 text-gray-400 font-medium text-lg py-2 px-4 mr-6"
-      >
-        Home
-      </Link>
-    </li>
-  );
+  <li>
+    <Link
+      to="/"
+      className=" rounded-md my-2 inline-block hover:bg-gray-700 no-underline bg-gray-800 p-1 text-gray-400 hover:text-white font-medium text-lg py-2 px-4 lg:-ml-2 mx-6"
+    >
+      Home
+    </Link>
+  </li>);
 
-  var home_open = (
-    <li>
-      <Link
-        to="/"
-        className=" rounded-md my-2 hover:bg-gray-700 inline-block no-underline hover:text-white bg-gray-800 p-1 text-gray-400 font-medium text-lg py-2 px-4 mr-6"
-      >
-        Home
-      </Link>
-    </li>
-  );
+  var home_open = 
+  <li>
+    <Link
+      to="/"
+      className=" rounded-md my-2 inline-block hover:bg-gray-700 no-underline bg-gray-800 p-1 text-gray-400 hover:text-white font-medium text-lg py-2 px-4 lg:-ml-2 mx-6"
+    >
+      Home
+    </Link>
+  </li>
 
-  var discussion = (
-    <li>
-      <Link
-        to="/"
-        className=" rounded-md my-2 inline-block hover:bg-gray-700 no-underline bg-gray-800 p-1 text-gray-400 hover:text-white font-medium text-lg py-2 px-4 lg:-ml-2 mx-6"
-      >
-        Discussions
-      </Link>
-    </li>
-  );
+  var discussion = 
+  <li>
+    <Link
+      to="/discussion"
+      className=" rounded-md my-2 inline-block hover:bg-gray-700 no-underline bg-gray-800 p-1 text-gray-400 hover:text-white font-medium text-lg py-2 px-4 lg:-ml-2 mx-6"
+    >
+      Discussions
+    </Link>
+  </li>
 
-  var discussion_open = (
-    <li>
-      <Link
-        to="/"
-        className=" rounded-md my-2 hover:bg-gray-700 inline-block no-underline hover:text-white bg-gray-800 p-1 text-gray-400 font-medium text-lg py-2 px-4 mr-6"
-      >
-        Discissions
-      </Link>
-    </li>
-  );
+  var discussion_open = 
+  <li>
+    <Link
+      to="/discussion"
+      className=" rounded-md my-2 inline-block hover:bg-gray-700 no-underline bg-gray-800 p-1 text-gray-400 hover:text-white font-medium text-lg py-2 px-4 lg:-ml-2 mx-6"
+    >
+      Discussions
+    </Link>
+  </li>
 
-  var settings = (
-    <li>
-      <Link
-        to="/"
-        className=" rounded-md my-2 inline-block hover:bg-gray-700 no-underline bg-gray-800 p-1 text-gray-400 hover:text-white font-medium text-lg py-2 px-4 lg:-ml-2 mx-6"
-      >
-        Settings
-      </Link>
-    </li>
-  );
+  var settings = 
+  <li>
+    <Link
+      to="/settings"
+      className=" rounded-md my-2 inline-block hover:bg-gray-700 no-underline bg-gray-800 p-1 text-gray-400 hover:text-white font-medium text-lg py-2 px-4 lg:-ml-2 mx-6"
+    >
+      Settings
+    </Link>
+  </li>
 
-  var settings_open = (
-    <li>
-      <Link
-        to="/"
-        className=" rounded-md my-2 hover:bg-gray-700 inline-block no-underline hover:text-white bg-gray-800 p-1 text-gray-400 font-medium text-lg py-2 px-4 mr-6"
-      >
-        settings
-      </Link>
-    </li>
-  );
+  var settings_open =
+  <li>
+    <Link
+    to="/settings"
+    className=" rounded-md my-2 inline-block hover:bg-gray-700 no-underline bg-gray-800 p-1 text-gray-400 hover:text-white font-medium text-lg py-2 px-4 lg:-ml-2 mx-6"
+    >
+      Settings
+    </Link>
+  </li>
 
   if (is_logged_in == true) {
     var sign_in;
@@ -77,7 +71,7 @@ export default function Navbar(props) {
     var sign_up = (
       <li className="absolute right-0">
         <Link
-          to="/"
+          to="/sign_up"
           className=" rounded-md my-2 hover:bg-gray-700 inline-block no-underline bg-gray-800 p-1 text-gray-400 hover:text-white font-medium text-lg py-2 px-4 lg:-ml-2 mx-6"
         >
           Profile
@@ -87,18 +81,19 @@ export default function Navbar(props) {
     var sign_up_open = (
       <li className="absolute right-0">
         <Link
-          to="/"
+          to="/sign_up"
           className=" rounded-md my-2 hover:bg-gray-700 inline-block no-underline bg-gray-800 p-1 text-gray-400 hover:text-white font-medium text-lg py-2 px-4 lg:-ml-2 mx-6"
         >
           Profile
         </Link>
       </li>
     );
-  } else {
+  }
+  else {
     var sign_in = (
       <li className="absolute right-24">
         <Link
-          to="/"
+          to="/sign_in"
           className=" rounded-md my-2 hover:bg-gray-700 inline-block no-underline hover:text-white bg-gray-800 p-1 text-gray-400 font-medium text-lg py-2 px-4 lg:-ml-2 mx-6"
         >
           Sign in
@@ -108,7 +103,7 @@ export default function Navbar(props) {
     var sign_in_open = (
       <li className="absolute right-24">
         <Link
-          to="/"
+          to="/sign_in"
           className=" rounded-md my-2 hover:bg-gray-700 inline-block no-underline hover:text-white bg-gray-800 p-1 text-gray-400 font-medium text-lg py-2 px-4 lg:-ml-2 mx-6"
         >
           Sign in
@@ -141,51 +136,48 @@ export default function Navbar(props) {
       <header className="hidden md:flex md:items-center md:w-auto w-full order-3 md:order-1">
         <nav className="bg-gray-800 h-auto w-screen content-start">
           <ul className="flow-root md:flex items-center text-base text-blue-600 pt-4 md:pt-0 w-screen">
-            <Link to="/">
               <img
                 src={logo}
-                className="inline-block mr-4"
                 height="5%"
                 width="5%"
               />
-            </Link>
             <Switch>
-              <Route exact path="/">
-                {home_open}
-                {discussion}
-                {settings}
-                {sign_in}
-                {sign_up}
-              </Route>
-              <Route exact path="/discussions">
-                {home}
-                {discussion_open}
-                {settings}
-                {sign_in}
-                {sign_up}
-              </Route>
-              <Route exact path="/settings">
-                {home}
-                {discussion}
-                {settings_open}
-                {sign_in}
-                {sign_up}
-              </Route>
-              <Route exact path="/sign_in">
-                {home}
-                {discussion}
-                {settings}
-                {sign_in_open}
-                {sign_up}
-              </Route>
-              <Route exact path="/sign_up">
-                {home}
-                {discussion}
-                {settings}
-                {sign_in}
-                {sign_up_open}
-              </Route>
-            </Switch>
+            <Route exact path="/">
+              {home_open}
+              {discussion}
+              {settings}
+              {sign_in}
+              {sign_up}
+            </Route>
+            <Route exact path="/discussion">
+              {home}
+              {discussion_open}
+              {settings}
+              {sign_in}
+              {sign_up}
+            </Route>
+            <Route exact path="/settings">
+              {home}
+              {discussion}
+              {settings_open}
+              {sign_in}
+              {sign_up}
+            </Route>
+            <Route exact path="/sign_in">
+              {home}
+              {discussion}
+              {settings}
+              {sign_in_open}
+              {sign_up}
+            </Route>
+            <Route exact path="/sign_up">
+              {home}
+              {discussion}
+              {settings}
+              {sign_in}
+              {sign_up_open}
+            </Route>
+          </Switch>
             {/* <img src={userimg} className="inline-block mr-4 absolute right-0" height="4%" width="4%"/> */}
           </ul>
         </nav>
