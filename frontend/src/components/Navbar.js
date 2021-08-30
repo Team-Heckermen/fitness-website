e435a3c16e7525fec796be21b76b33bb7031d162
madara uchiha <pritanshsahsani@gmail.com>
2021-08-30 14:36:24 +0530

    navbar + sign in edits

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -4,38 +4,24 @@ import search from "../assets/images/search_icon.svg";
 // import userimg from "../assets/images/default_user_img.svg";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
+var navigation = [
+  { num:0, name: 'Home', href: '/', current: true },
+  { num:1, name: 'Discussions', href: '/discussion', current: false },
+  { num:2, name: 'Settings', href: '/settings', current: false },
+]
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
+
+function Change_current_of(clicked_on){
+  navigation[0].current=false
+  console.log(navigation[clicked_on].current)
+}
+
 export default function Navbar(props) {
   const is_logged_in = false;
   var button_style=" rounded-md my-2 hover:bg-gray-700 inline-block no-underline bg-gray-800 text-gray-400 hover:text-white font-medium text-lg py-2 px-4 lg:-ml-2 mx-6"
-  var home = (
-  <li>
-    <Link
-      to="/"
-      className={button_style}
-    >
-      Home
-    </Link>
-  </li>);
-
-  var discussion = 
-  <li>
-    <Link
-      to="/discussion"
-      className={button_style}
-    >
-      Discussions
-    </Link>
-  </li>
-
-  var settings = 
-  <li>
-    <Link
-      to="/settings"
-      className={button_style}
-    >
-      Settings
-    </Link>
-  </li>
 
   if (is_logged_in == true) {
     var sign_in;
@@ -83,23 +69,34 @@ export default function Navbar(props) {
                 width="5%"
                 className="mr-10 ml-4 py-2"
               />
-              {home}
-              {discussion}
-              {settings}
-              {sign_in}
-              {sign_up}
+              {navigation.map((item) => (
+                <Link
+                  onClick={() => Change_current_of(item.num)}
+                  key={item.name}
+                  to={item.href}
+                  className={classNames(
+                  item.current ? 'bg-gray-900 text-white' : "hover:bg-gray-700 bg-gray-800 text-gray-400 hover:text-white",
+                  "rounded-md my-2 inline-block no-underline font-medium text-lg py-2 px-4 lg:-ml-2 mx-6"
+                        )}
+                        aria-current={item.current ? 'page' : undefined}
+                      >
+                        {item.name}
+                      </Link>
+              ))}
               
-              <div class="p-2 ml-20">
-                <div class="bg-white flex items-center rounded-full shadow-xl">
-                  <input class="rounded-l-full w-full py-2 px-6 text-gray-700 leading-tight focus:outline-none" id="search" type="text" placeholder="Search" />
-                  <div class="p-2 py-0">
-                    <button class="bg-gray-800 text-white rounded-full p-1 hover:bg-gray-900 focus:outline-none w-6 h-6 flex items-center justify-center">
+              <div className="p-2 ml-20">
+                <div className="bg-white flex items-center rounded-full shadow-xl">
+                  <input className="rounded-l-full w-full py-2 px-6 text-gray-700 leading-tight focus:outline-none" id="search" type="text" placeholder="Search" />
+                  <div className="p-2 py-0">
+                    <button className="bg-gray-800 text-white rounded-full p-1 hover:bg-gray-900 focus:outline-none w-6 h-6 flex items-center justify-center">
                       <img src={search} height="100%" width="100%"/>
                     </button>
                   </div>
                 </div>
               </div>
             {/* <img src={userimg} className="inline-block mr-4 absolute right-0" height="4%" width="4%"/> */}
+            {sign_in}
+            {sign_up}
           </ul>
         </nav>
       </header>
--- frontend/src/components/sign_in.js
+++ frontend/src/components/sign_in.js
@@ -4,22 +4,23 @@ import search from "../assets/images/search_icon.svg";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 export default function Sign_in(props) {
+    var button_style="block py-3 px-5 mx-auto mt-16 text-center align-text-top w-1/2 rounded-full border-2 border-gray-800"
   return (
       <>
-        <div className="w-1/2 inline-block">
-            <form>
+        <div className="w-1/2">
+            <form className="space-y-15">
                 <input
-                    className="block py-3 px-5 mx-auto mt-16 text-center align-text-top w-1/2 h-15 rounded-full border-2 border-gray-800"
+                    className={button_style}
                     type="text" 
                     placeholder="Email"
                 />
                 <input
-                    className="block py-3 px-5 mx-auto mt-16 text-center align-text-top w-1/2 h-15 rounded-full border-2 border-gray-800"
+                    className={button_style}
                     type="password" 
                     placeholder="password"
                 />
                 <input
-                    className="block py-3 px-5 mx-auto mt-16 text-center align-text-top w-1/4 h-15 rounded-full text-white   bg-gray-800"
+                    className={button_style}
                     type="submit" 
                     placeholder="Submit"
                 />                
