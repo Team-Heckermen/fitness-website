4830080030d0ea8c95eacf43944f177060d688d6
Dheer <dheerm309@gmail.com>
2021-08-30 20:08:37 +0530

    changed function to arrow function

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,16 +1,13 @@
 import React from "react";
-import search from "../assets/images/search_icon.svg";
-// import userimg from "../assets/images/default_user_img.svg";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-export default function Footer(props) {
+export const Footer = (props) => {
   return (
-      <>
-        <div className="absolute inset-x-0 bottom-0 py-3 text-sm text-white bg-gray-800">
-            <p className="w-screen text-center">
-                &copy; 2021 Healthify. All rights reserved.
-            </p>
-        </div>
-      </>
+    <>
+      <div className="absolute inset-x-0 bottom-0 py-3 text-sm text-white bg-gray-800">
+        <p className="w-screen text-center">
+          &copy; 2021 Healthify. All rights reserved.
+        </p>
+      </div>
+    </>
   );
-}
+};
