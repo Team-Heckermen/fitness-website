678ba5068f6316470678835901ffaa2b1099706d
madara uchiha <pritanshsahsani@gmail.com>
2021-08-30 12:51:45 +0530

    footer edits

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,5 +1,4 @@
 import React from "react";
-import logo from "../assets/images/logo.svg";
 import search from "../assets/images/search_icon.svg";
 // import userimg from "../assets/images/default_user_img.svg";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
@@ -7,9 +6,9 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 export default function Footer(props) {
   return (
       <>
-        <div className="absolute inset-x-0 bottom-0 h-5 text-xs text-white bg-gray-800">
+        <div className="absolute inset-x-0 bottom-0 py-3 text-sm text-white bg-gray-800">
             <p className="w-screen text-center">
-                &copy 2021 "appname". All rights reserved.
+                &copy; 2021 Healthify. All rights reserved.
             </p>
         </div>
       </>
