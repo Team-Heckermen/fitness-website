3c29137a5ad10b8453c54bb114360dbf2e27406f
madara uchiha <pritanshsahsani@gmail.com>
2021-08-30 20:06:12 +0530

    discussion model edit

--- api/models.py
+++ api/models.py
@@ -53,7 +53,7 @@ class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
-    author = models.ForeignKey(UserAccount)
+    author = models.ForeignKey(UserAccount, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
