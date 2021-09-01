4a6c0a27b872087e61d884de5fcebead0950007f
madara uchiha <pritanshsahsani@gmail.com>
2021-09-01 10:30:26 +0530

    eddited views + urls for discussions

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,13 @@
+from api.views import apiOverview
 from django.contrib import admin
 from django.urls import path, include, re_path
 from django.views.generic import TemplateView
+from . import views
 
 urlpatterns = [
+    path('', views.apiOverview ,name="api-overview"),
     path('admin/', admin.site.urls),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
-    path('api/', include('api.urls'))
+    path('list_post/', views.List_post, name="api-overview"),
 ]
-
--- api/views.py
+++ api/views.py
@@ -1,3 +1,25 @@
+from rest_framework import serializers
+from api.models import Post
 from django.shortcuts import render
+from django.http import JsonResponse, response
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import PostSerializer
 
-# Create your views here.
+@api_view(['GET'])
+def apiOverview(request):
+  api_urls = {
+    'user_details' : '/user_details/',
+
+    'List_Post' : '/list_post/',
+    'Detail_view_Post' : '/detail_view/<str:pk>/',
+    'Create_Post' : '/create_post/',
+    'Update_Post' : '/update_post/<str:pk>/',
+    'delete_Post' : '/delete_post/<str:pk>/',
+  }
+  return Response(api_urls)
+
+def List_post(request):
+  Posts = Post.objects.all()
+  serializer = PostSerializer(Posts, many=True)
+  return Response(serializer.data)
