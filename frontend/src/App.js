3c442313cbc38e7d6663590138ee8507bbbf0eaf
Dheer <dheerm309@gmail.com>
2021-09-08 22:08:15 +0530

    update imports

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import { Navbar } from "./components/Navbar";
 import { Footer } from "./components/Footer";
-import Sign_in from "./components/sign_in";
+import { Sign_in } from "./components/sign_in";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 function App() {
   return (
