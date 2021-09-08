57a89021cf4e2d219d427bf32034de2d1728f6df
Dheer <dheerm309@gmail.com>
2021-09-08 22:08:36 +0530

    make the sign in form

--- frontend/src/components/sign_in.js
+++ frontend/src/components/sign_in.js
@@ -3,31 +3,34 @@ import search from "../assets/images/search_icon.svg";
 // import userimg from "../assets/images/default_user_img.svg";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-export default function Sign_in(props) {
-    var button_style="block py-3 px-5 mx-auto mt-16 text-center align-text-top w-1/2 rounded-full border-2 border-gray-800"
+export const Sign_in = (props) => {
   return (
-      <>
-        <div className="w-1/2">
-            <form className="space-y-15">
-                <input
-                    className={button_style}
-                    type="text" 
-                    placeholder="Email"
-                />
-                <input
-                    className={button_style}
-                    type="password" 
-                    placeholder="password"
-                />
-                <input
-                    className={button_style}
-                    type="submit" 
-                    placeholder="Submit"
-                />                
-            </form>
+    <>
+      <div className="flex">
+        <div class="sign-in-container">
+          <div class="h-2 bg-pink-600 rounded-t-md"></div>
+          <div class="px-8 py-6">
+            <label class="block font-semibold">Email</label>
+            <input
+              type="text"
+              placeholder="example@email.com"
+              class="sign-in-input"
+            />
+            <label class="block mt-3 font-semibold">Password</label>
+            <input
+              type="password"
+              placeholder="password123"
+              class="sign-in-input"
+            />
+            <div class="flex justify-between items-baseline">
+              <button class="primary-btn">Login</button>
+              <Link to="/forgot-password" class="text-sm hover:bg-pink-100 ">
+                Forgot password?
+              </Link>
+            </div>
+          </div>
         </div>
-        <div className="w-1/2 inline-block">
-        </div>
-      </>
+      </div>
+    </>
   );
-}
+};
