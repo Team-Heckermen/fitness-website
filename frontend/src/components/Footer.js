386262e20fa10fff3bef0e4a427afc474e010fa6
madara uchiha <pritanshsahsani@gmail.com>
2021-09-20 22:39:59 +0530

    footer glitch 2 fix

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 export const Footer = () => {
   return (
     <>
-      <footer className="fixed bg-white inset-x-0 bottom-0 py-3 text-sm text-gray-900">
+      <footer className="fixed bg-white inset-x-0 bottom-0 pb-6 text-sm text-gray-900 ">
         <hr className="w-95% mx-auto mb-2 border-gray-500"/>
         <p className="w-screen text-center">
           &copy; 2021 Healthify. All rights reserved.
