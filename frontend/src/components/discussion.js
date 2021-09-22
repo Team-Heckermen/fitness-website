be1741b0540cc7082e1e5cd84ab04a9d907b2447
madara uchiha <pritanshsahsani@gmail.com>
2021-09-22 09:39:11 +0530

    Revert "margin errror solved"
    This reverts commit 67b627f8d8fa1c9673d3e988508a179c619c8da8.

--- frontend/src/components/discussion.js
+++ frontend/src/components/discussion.js
@@ -8,10 +8,10 @@ export const DiscussionDisplay = (props) => {
       {
         posts.map(post => (
           <div className="discussion-display-container w-90%">
-            <div className="h-2 bg-blue-400 rounded-t-md mb-2"></div>
-            <a className="mr-2 ml-6 text-blue-400" href="#">{ post[3]}</a>
-            <small className="text-muted">{ post[0] }</small>
-            <hr className="w-98% mx-auto my-1" />
+          <div className="h-2 bg-blue-400 rounded-t-md mb-2"></div>
+              <a className="mr-2 ml-6 text-blue-400" href="#">{ post[3]}</a>
+              <small className="text-muted">{ post[0] }</small>
+              <hr className="w-98% mx-auto my-1" />
             <p className="text-2xl ml-6">{post[1]}</p>
             <p className="text-lg ml-6">{post[2]}</p>
           </div>
