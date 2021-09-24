09bc57bba4e83aa06e5864242ff1bb22b94447e9
Dheer <dheerm309@gmail.com>
2021-09-24 15:11:17 +0530

    blacklist bug solve

--- core/settings.py
+++ core/settings.py
@@ -148,6 +148,7 @@ SIMPLE_JWT = {
     'AUTH_HEADER_TYPES': ('JWT',),
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'BLACKLIST_AFTER_ROTATION': False,
 }
 
 # Default primary key field type
