f38d3ec2dcfc58ce3a3cc6b5733f09d34e265e76
Dheer <dheerm309@gmail.com>
2021-09-28 00:58:56 +0530

    fixed the password reset confirmation form bug

--- frontend/src/components/ResetPasswordConfirm.js
+++ frontend/src/components/ResetPasswordConfirm.js
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
 import { Redirect } from "react-router-dom";
 import { connect } from "react-redux";
-import { reset_password_confirm } from "../actions/auth.js";
+import { reset_password_confirm } from "../actions/auth";
 
 const ResetPasswordConfirm = ({ match, reset_password_confirm }) => {
+  const [requestSent, setRequestSent] = useState(false);
   const [formData, setFormData] = useState({
     new_password: "",
     re_new_password: "",
@@ -21,52 +22,48 @@ const ResetPasswordConfirm = ({ match, reset_password_confirm }) => {
     const token = match.params.token;
 
     reset_password_confirm(uid, token, new_password, re_new_password);
+    setRequestSent(true);
   };
 
-  // is the user authenticated? yes then redirect to homepage
-  if (match) {
+  if (requestSent) {
     return <Redirect to="/" />;
   }
 
   return (
-    <>
-      <div className="flex">
-        <div className="sign-in-container">
-          <div className="h-2 bg-blue-400 rounded-t-md"></div>
-          <form className="px-8 py-6" onSubmit={(e) => onSubmit(e)}>
-            <label className="block mt-3 font-semibold">New Password</label>
-            <input
-              type="password"
-              placeholder="password123"
-              className="sign-in-input"
-              name="new-password"
-              value={new_password}
-              onChange={(e) => onChange(e)}
-              minLength="7"
-              required
-            />
-            <label className="block mt-3 font-semibold">
-              Confirm New Password
-            </label>
-            <input
-              type="password"
-              placeholder="password123"
-              className="sign-in-input"
-              name="confirm-new-password"
-              value={re_new_password}
-              onChange={(e) => onChange(e)}
-              minLength="7"
-              required
-            />
-            <div className="flex justify-between items-baseline">
-              <button className="primary-btn" type="submit">
-                Reset Password
-              </button>
-            </div>
-          </form>
-        </div>
+    <div className="flex">
+      <div className="sign-in-container">
+        <div className="h-2 bg-blue-400 rounded-t-md"></div>
+        <form className="px-8 py-6" onSubmit={(e) => onSubmit(e)}>
+          <label className="block font-semibold">Password</label>
+          <input
+            type="password"
+            placeholder="password123"
+            className="sign-in-input"
+            name="new_password"
+            value={new_password}
+            onChange={(e) => onChange(e)}
+            minLength="7"
+            required
+          />
+          <label className="block mt-3 font-semibold">Confirm Password</label>
+          <input
+            type="password"
+            placeholder="password123"
+            className="sign-in-input"
+            name="re_new_password"
+            value={re_new_password}
+            onChange={(e) => onChange(e)}
+            minLength="7"
+            required
+          />
+          <div className="flex justify-between items-baseline">
+            <button className="primary-btn" type="submit">
+              Reset Password
+            </button>
+          </div>
+        </form>
       </div>
-    </>
+    </div>
   );
 };
 
