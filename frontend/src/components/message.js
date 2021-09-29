84cee314ca4e9c63fbd26a1b2483e6d43faf5412
madara uchiha <pritanshsahsani@gmail.com>
2021-09-29 11:16:38 +0530

    further simplified message.js

--- frontend/src/components/message.js
+++ frontend/src/components/message.js
@@ -3,13 +3,6 @@ import React from "react";
 export const Message = (props) => {
   if(props.message != null)
   {
-    var message_bol = true
-  }
-  else{
-    var message_bol = false
-  }
-  if(message_bol)
-  {
     return (
       <div className="mx-auto w-95% rounded-md bg-blue-400">
         <p className=" rounded-md py-0.5 text-sm w-full text-center">{props.message}</p>
