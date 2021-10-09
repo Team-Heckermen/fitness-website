0122df03ac2d426e66746b7afa9e4e7d3588d026
Dheer <dheerm309@gmail.com>
2021-10-10 00:32:34 +0530

    made logout working

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../assets/images/logo.png";
 import search from "../assets/images/search_icon.svg";
 import userimg from "../assets/images/default_user_img.svg";
@@ -7,6 +7,8 @@ import { logout } from "../actions/auth";
 import { connect } from "react-redux";
 import { Link, Redirect } from "react-router-dom";
 const Navbar = ({ toggle, logout, isAuthenticated }) => {
+  const [redirect, setRedirect] = useState(false);
+
   const guestLinks = () => (
     <>
       <Link to="/sign_in" className="p-1">
@@ -23,7 +25,7 @@ const Navbar = ({ toggle, logout, isAuthenticated }) => {
       <Link className="navbar-link" to="/profile">
         <button className="underline-btn">Profile</button>
       </Link>
-      <Link className="navbar-link mr-4" to="/logout" onClick={logoutHandler}>
+      <Link className="navbar-link mr-4" to="/" onClick={logoutHandler}>
         <button className="underline-btn">Logout</button>
       </Link>
     </>
@@ -31,7 +33,7 @@ const Navbar = ({ toggle, logout, isAuthenticated }) => {
 
   const logoutHandler = () => {
     logout();
-    return <Redirect to="/" />;
+    setRedirect(true);
   };
   return (
     <div>
@@ -89,6 +91,7 @@ const Navbar = ({ toggle, logout, isAuthenticated }) => {
         </div>
       </nav>
       <hr className="w-95% mx-auto m-0 p-0 border-gray-700" />
+      {redirect ? <Redirect to="/" /> : <></>}
     </div>
   );
 };
