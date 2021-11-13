4217e54e46b28f12326e83e4aa88bb8a9eb7648e
pritansh sahsani <82302289+pritansh-sahsani@users.noreply.github.com>
2021-11-13 22:01:51 +0530

    404 error UI bug fixed

--- frontend/src/components/PageNotFound.js
+++ frontend/src/components/PageNotFound.js
@@ -1,10 +1,18 @@
 import React from "react";
 import message from "../assets/images/404page.gif";
 const PageNotFound = () => {
+  var { innerWidth: width, innerHeight: height } = window;
+  var mheight = height - 130;
+  height = (width/16)*9;
+  if(height>mheight)
+  {
+    height = mheight;
+    width = (height/9)*16;
+  }
   return (
     <div>
       <title>YourHealthPal - page not found</title>
-      <img src={message} className=" max-w-4xl mx-auto" />
+      <img src={message} width={width} className="mx-auto" />
     </div>
   );
 };
